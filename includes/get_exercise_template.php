46c284579798a7d13849627b40bd842b762a0a17
Maarten Hormes <I456806@fhict.nl>
2021-04-08 10:30:06 +0200

    content update

--- includes/get_exercise_template.php
+++ includes/get_exercise_template.php
@@ -1,16 +1,34 @@
 <?php
-function GetAllExersices()
+function GetAllExercises($catName)
 {
     include '../includes/connection_template.php';
     try{
 
         $conn = new PDO("mysql:host=studmysql01.fhict.local;dbname=dbi459847",$username, $password);
-        $sql = 'SELECT * FROM exercise';
+        $sql = 'SELECT * FROM exercise WHERE MuscleTrained = :catName';
+        $sth = $conn->prepare($sql);
+        $sth->execute([':catName' => $catName]);
 
-        $query = $conn->query($sql);
-        $exercises = $query->fetchAll(PDO::FETCH_OBJ);
+        $exercises = $sth->fetchAll(PDO::FETCH_OBJ);
 
         return $exercises;
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
+
+function GetChosenExercise($exName)
+{
+    include '../includes/connection_template.php';
+    try{
+
+        $conn = new PDO("mysql:host=studmysql01.fhict.local;dbname=dbi459847",$username, $password);
+        $sql = 'SELECT * FROM exercise WHERE Name = :exName';
+        $sth = $conn->prepare($sql);
+        $sth->execute([':exName' => $exName]);
+
+        $exercise = $sth->fetchAll(PDO::FETCH_OBJ);
+
     }catch(PDOException $e){
         echo $e->getMessage();
     }
