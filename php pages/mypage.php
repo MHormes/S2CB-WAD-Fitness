1f953ef29d776cc5c3082b965b8a9779e4baaf2d
Alessandro Busacchi <I459847@fhict.nl>
2021-04-11 14:38:21 +0200

    update informations user almost done

--- php pages/mypage.php
+++ php pages/mypage.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+include '../includes/get_user.php';
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,6 +11,7 @@ session_start();
         <meta name="description" content="Fintess website">
         <title>AM Fitness</title>
         <link rel="stylesheet" type="text/css" href="../resources/css/main.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/login.css">
     </head>
     <body>
         <div class="grid-container">
@@ -21,7 +24,39 @@ session_start();
         </div>
         <?php
         if(isset($_SESSION["Username"])){
-            echo "Welcome to your own page, " . $_SESSION["Username"]; 
+            $UserUsername = $_SESSION['Username'];
+            $newUser = GetUserDetails($UserUsername);
+            echo "Welcome to your own page, " . $_SESSION["Username"]; ?>
+            <div class="row">
+                <form id="update" method="post" action="mypage.php" class="login-form">
+                    <!-- inserting first name, second name, username, password and confirm password -->
+                    <div class="row">
+                        <label for="fName">First name</label>
+                        <input type="text" name="fname" id="fname" value=<?php echo $newUser->fName; ?> required>
+                    </div>
+                    <div class="row">
+                        <label for="lName">Last name</label>
+                        <input type="text" name="lname" id="lname" value=<?php echo $newUser->lName; ?> required>
+                    </div>
+                    <div class="row">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" value=<?php echo $newUser->Username; ?> required>
+                    </div>
+                    <div class="row">
+                        <label for="pwd">Password</label>
+                        <input type="password" name="password" id="password" value=<?php echo $newUser->Password; ?> required>
+                    </div>
+                    <div class="row">
+                        <label for="email">Email</label>
+                        <input type="text" pattern="^[A-z0-9._-]+@[A-z0-9._-]+.[a-z]+" name="email" id="email" value=<?php echo $newUser->Email; ?> required>
+                    </div>
+                    <div class="row">
+                        <input type="submit" value="Update" name="btnUpdate">
+                    </div>
+                </form>
+            </div>
+        
+        <?php
         }
         else{
             echo "To see your page please log-in";
